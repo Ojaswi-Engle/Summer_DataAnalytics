ea82ee79dfb9144e91e667a60f9530c4a0a5d156
Ojaswi-Engle <ojaswiengle@gmail.com>
2026-06-12 16:21:41 +0530

    Create Operators.py

---
+++ Python_learning/Day_2/Operators.py
@@ -0,0 +1,46 @@
+#arithmetic operators
+a = 3 
+b = 2
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+#relational
+print(a<b)
+print(a<=b)
+print(a>b)
+print(a>=b)
+print(a==b)
+print(a!=b)
+
+#assignment 
+a+=1 #-=,*=,/=,//=,**=,%=
+print(a)
+
+#logical 
+print(0 and 1)
+print(1 or 0)
+print(not a)
+
+#biwise
+
+print(a & b)
+print(a | b)
+print(a^b)
+print(a<<b)
+print(a>>b)
+print(~a)
+
+#memebership 
+print('D' in 'Delhi')
+print('D' in '@elhi')
+print('D' not in 'Delhi')
+
+#identity
+print(a is b)
+print(a is not b)
