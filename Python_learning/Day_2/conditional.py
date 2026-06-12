021626d6c7eaa05be7b00088391b46c7d92340db
Ojaswi-Engle <ojaswiengle@gmail.com>
2026-06-12 16:23:25 +0530

    Update conditional.py

--- Python_learning/Day_2/conditional.py
+++ Python_learning/Day_2/conditional.py
@@ -1,1 +1,17 @@
+#login credentials
+#password checking 
 
+email=input("enter mail_id:")
+password=input("enter password:")
+
+if email=='xyz@gmail.com' and password =='12345':
+    print("WELCOME")
+elif email=='xyz@gmail.com' and password !='12345':
+    print("INCORRECT PASSWORD")
+    password=input("enter password again:")
+    if password=='12345':
+        print('WELCOME')
+    else:
+        print('ERROR')
+else:
+    print("ERROR")
