f8ed8fe70c829f4e0a3d639bd75ee2924784853b
Ojaswi-Engle <ojaswiengle@gmail.com>
2026-06-12 16:09:43 +0530

    Create Introduction.py

---
+++ Python_learning/Day_1/Introduction.py
@@ -0,0 +1,17 @@
+print('hello world')
+print('ojaswi engle')
+print(7)
+print(True)
+print('ojaswi engle',21,5.6,True)
+# use of separator sep.
+print('ojaswi engle',21,5.6,True,sep='/')
+
+# use of end . 
+print('hello')# default end='\n'
+print('world')
+
+print('hello',end=' + ')
+print('world')
+
+#type function 
+print(type(4.5))
