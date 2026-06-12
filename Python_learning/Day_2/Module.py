1acdc3ba3585762554e17f5e99d2c3761bf83521
Ojaswi-Engle <ojaswiengle@gmail.com>
2026-06-12 16:50:51 +0530

    Create Module.py

---
+++ Python_learning/Day_2/Module.py
@@ -0,0 +1,18 @@
+#modules- files which contain standard predefined functions
+#math
+import math
+print(math.factorial(5))
+print(math.sqrt(36))
+
+#keword
+import keyword
+print(keyword.kwlist)
+
+#random
+import random
+print(random.randint(1,50))
+
+#datetime
+import datetime
+
+print(datetime.datetime.now())
