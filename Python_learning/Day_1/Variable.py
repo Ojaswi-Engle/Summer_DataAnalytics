d911d73d68c433ca723991a589d1e8954a8a6a47
Ojaswi-Engle <ojaswiengle@gmail.com>
2026-06-12 16:11:06 +0530

    Create Variable.py

---
+++ Python_learning/Day_1/Variable.py
@@ -0,0 +1,34 @@
+a=4
+print(a)
+
+#dynamically typed vs statically typed
+#c/c++ int a =4;
+
+name='ojaswi'
+print(name)
+
+#dynamic binding vs static binding 
+#static int a=4;  a!= 'hello'
+a=2
+print(a)
+
+a='hello'
+print(a)
+
+#stylish declaration 
+a=2
+b=4
+c=6
+print(a,b,c)
+x,y,z=2,5,7
+print(x,y,z)
+
+m=n=p=9
+print(m,n,p)
+
+#input
+
+
+
+
+
