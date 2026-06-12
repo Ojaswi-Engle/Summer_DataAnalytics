17009ec31f288aa49d3cc793b05e39bc0558cf94
Ojaswi-Engle <ojaswiengle@gmail.com>
2026-06-12 16:25:18 +0530

    Create Minimum_Ofthreenumbers.py

---
+++ Python_learning/Day_2/Minimum_Ofthreenumbers.py
@@ -0,0 +1,12 @@
+# minimum of 3 numbers
+
+a=int(input("enter first number:"))
+b=int(input("enter second number:"))
+c=int(input("enter third number:"))
+
+if a<b and a<c:
+    print(a," is minimum")
+elif b<a and b<c:
+    print(b," is minimum")
+else:
+    print(c," is minimum")
