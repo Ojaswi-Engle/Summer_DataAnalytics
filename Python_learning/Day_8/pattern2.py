6d5ac8ed7dbd43444aeeecf1b471554e8adefc57
Ojaswi-Engle <ojaswiengle@gmail.com>
2026-06-26 07:40:04 +0530

    Create pattern2.py

---
+++ Python_learning/Day_8/pattern2.py
@@ -0,0 +1,23 @@
+'''Problem 2: Print the following pattern.
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+* * * * 
+* * * 
+* * 
+*'''
+
+n=int(input("enter a number:"))
+
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print('*',end='')
+    print()
+for i in range(1,n):
+    for j in range(n-i,0,-1):
+        print('*',end='')
+    print()
+
+
